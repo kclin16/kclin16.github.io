695e478dd58f6d2faee8579c46ed1e41143b501f
kclin16 <kclin16@gmail.com>
2025-02-23 14:17:32 -0500

    Adjust names. add ;

--- ComicReader/reader.js
+++ ComicReader/reader.js
@@ -414,9 +414,9 @@ function load_global(){
 
 function load_book_settings(){
     if(loaded_book){
-        let settings = localStorage.getItem(`comic_${loaded_book}`);
-        if(settings){
-            settings = settings.split(' ')
+        const settings_data = localStorage.getItem(`comic_${loaded_book}`);
+        if(settings_data){
+            settings = settings_data.split(' ');
             console.log(`Loading: ${settings}`);
             is_rtl = settings[1] === 'true';
             document.getElementById('rtl').checked = settings[1] === 'true';
